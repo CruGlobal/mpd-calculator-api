89b307e151c8d3f0a065c2d77c6e334c71938423
davidviolleau <david.violleau@agapefrance.org>
2016-07-19 09:45:00 -0400

    Added mpd_budget route for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  resources :forms
-  resources :budgets
+
+  # MInistries end point
   resources :ministries
+
+  # Forms end point
+  resources :forms
+
+  # Budgets end point
+  resources :budgets
+  get '/mpd_budget', to: 'budgets#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
