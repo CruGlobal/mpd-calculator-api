1444691262d45fcf72163aab1ebdc25bcd172c6e
MayWorkman <may.workman@cru.org>
2016-07-19 10:14:03 -0400

    most business logic is here but no ministry filter

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,7 +1,13 @@
 class FormsController < ApplicationController
   def index
-    #TODO filter by ministry id
-    @question_sheets = Fe::QuestionSheet.all
+
+    #TODO token logic is this really optional?
+    token = params[:token]
+
+    refresh_forms if params[:refresh] == 'true'
+    filter_by_ministries
+    filter_inactive unless params[:show_inactive] == 'true'
+    filter_global unless params[:include_global] == 'true'
 
   end
 
@@ -23,4 +29,32 @@ class FormsController < ApplicationController
 
   end
 
+  private
+
+  def filter_by_ministries
+    #TODO filter by ministry id
+    ministry_id = params[:ministry_id]
+    @forms = Form.all
+    Rails.logger.debug 'filter_by_ministries'
   end
+
+  #filter returns only active forms
+  def filter_inactive
+    @forms = @forms.where(archived: false)
+    Rails.logger.debug 'filter_inactive'
+  end
+
+  #filter return only non-global forms
+  def filter_global
+    @forms = @forms.where(is_global: false)
+    Rails.logger.debug 'filter_global'
+  end
+
+  def refresh_forms
+    #TODO refresh parameter
+  end
+
+
+end
+
+
